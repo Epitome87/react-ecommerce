6c61ad8866d851bbcfc5eceac3c96bd6871585c8
Matthew McGrath <rubywep@hotmail.com>
2022-01-02 00:48:35 -0800

    Finish up the handleSubmit method in the SignIn component. It makes an async call to auth.signInWithEmailAndPassword.

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -3,16 +3,21 @@ import FormInput from "../form-input/FormInput";
 import Button from "../button/Button";
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
-
+import { auth } from "../../firebase/firebase.utils";
 function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    setEmail("");
-    setPassword("");
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setEmail("");
+      setPassword("");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleChange = (event) => {
