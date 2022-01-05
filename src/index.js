e6128a650465d23e3fa88e74bd41328bf57328fa
Matthew McGrath <rubywep@hotmail.com>
2022-01-04 16:13:12 -0800

    Add redux-persist to app, implementing it so the Redux state for user's Cart persists on browser refresh.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
 import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './userSlice';
 import cartReducer from './cartSlice';
 
+const reducers = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
