39000ca4ed34dece90d91eadcaa77266b69cdbaf
Matthew McGrath <rubywep@hotmail.com>
2022-03-20 16:17:00 -0700

    Tweak animation played when cart changes

--- client/src/components/cart/icon/CartIcon.styles.jsx
+++ client/src/components/cart/icon/CartIcon.styles.jsx
@@ -1,11 +1,13 @@
 import styled, { css, keyframes } from 'styled-components';
 
+const initialScale = 1.3;
+
 const bumpAnimation = keyframes`
-  0% { transform: scale(1); }
-  10% { transform: scale(0.85); }
-  30% { transform: scale(1.15); }
-  50% { transform: scale(1.2); }
-  100% { transform: scale(1); }
+  0% { transform: scale(${initialScale}); }
+  10% { transform: scale(${initialScale - 0.15}); }
+  30% { transform: scale(${initialScale + 0.15}); }
+  50% { transform: scale(${initialScale + 0.2}); }
+  100% { transform: scale(${initialScale}); }
 `;
 
 // If we use "isAnimating" prop
@@ -17,8 +19,12 @@ const animate = css`
 export const CartIcon = styled.div.attrs((props) => ({
   className: props.className,
 }))`
-  width: 45px;
-  height: 45px;
+  // width: 45px;
+  // height: 45px;
+
+  transform: scale(1.3);
+  margin-right: 1rem;
+
   position: relative;
   display: flex;
   align-items: center;
@@ -43,5 +49,11 @@ export const ItemCount = styled.span`
   position: absolute;
   font-size: 10px;
   font-weight: bold;
-  bottom: 12px;
+  // bottom: 12px;
+
+  top: -30%;
+  right: -30%;
+  padding: 2px;
+  background-color: #5b85aa;
+  border-radius: 50%;
 `;
