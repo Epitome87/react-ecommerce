3e5a524ce7ae539191fade53362e899f2f7b1b78
Matthew McGrath <rubywep@hotmail.com>
2022-03-20 21:06:24 -0700

    Add a Container class to globals

--- client/src/global.styles.jsx
+++ client/src/global.styles.jsx
@@ -8,19 +8,29 @@ export const GlobalStyle = createGlobalStyle`
 }
 
 body {
-  font-family: 'Montserrat';
-  padding: 1.5rem 4.5rem;
-  background-color: #252627;
-  color: white;
+  font-family: 'Kumbh Sans', sans-serif; //'Montserrat';
+  line-height: 1.5;
+  // padding: 0.75rem 1.25rem;
+  // background-color: #252627;
+  background-color: white;
+  color: black;
+  color: hsl(219, 9%, 45%);
   
 
-  @media screen and (max-width: 800px) {
-      padding: 0.75rem 1.25rem;
+  @media screen and (min-width: 800px) {
+        // padding: 1.5rem 4.5rem;
   }
 }
 
 a {
   text-decoration: none;
-  color: white;
+  // color: white;
+  color: black;
+}
+
+.container {
+  max-width: 1200px;
+  padding: 1rem;
+  margin: 0 auto;
 }
 `;
